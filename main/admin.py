62f45af9070ee9ed5367a976a7dd6b2409609255
Jakut <jakut040702@gmail.com>
2022-01-04 21:23:09 +0600

    blog125

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
 
-from .models import Category, Tag, Post
+from .models import Category, Tag, Post, PostImage
+
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    fields = ['image']
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [PostImageInline]
+
 
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 
--- main/views.py
+++ main/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from rest_framework .generics import ListAPIView
 
-# Create your views here.
+#TODO: список категорий
+#TODO: КРАД постов
+#TODO: картинки в постах
+#TODO: комменты
+#TODO: подключить Твилио
+#TODO: авторизация
+#TODO: избранное, лайки
+from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin, CreateModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+
+from main.models import Category, Post, Comment
+from main.permissions import IsAuthor
+from main.serializers import CategorySerializer, PostSerializer, PostListSerializer, CommentSerializer
+
+
+class CategoriesListView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_serializer_class(self):
+        serializer_class = super().get_serializer_class()
+        if self.action == 'list':
+            serializer_class = PostListSerializer
+        return serializer_class
+
+    def get_permissions(self):
+        #создавать пост может залогиненый юзер
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        #изменять или удалять только автор
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsAuthor()]
+
+        #просматривать мгут все
+
+        pass
+
+
+class CommentViewSet(CreateModelMixin,
+                     UpdateModelMixin,
+                     DestroyModelMixin,
+                     GenericViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
